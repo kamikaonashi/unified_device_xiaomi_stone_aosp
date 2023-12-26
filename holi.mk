681f66f345f1740121ded1a6ea68bf4a1aa065a3
kamikaonashi <thorschi93@gmx.at>
2023-12-26 07:21:04 +0100

    sm6375-common: add back audio_effects.xml
    why was it gone in the first place?

--- holi.mk
+++ holi.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_effects.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/bluetooth_audio_policy_configuration_7_0.xml \
 
 # Bluetooth
