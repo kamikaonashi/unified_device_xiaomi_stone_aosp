71ed9cf1b31369c138f59349031e2d4fc4f1760c
kamikaonashi <thorschi93@gmx.at>
2023-12-30 16:17:35 +0100

    sm6375-common: switch to AOSP WFD

--- holi.mk
+++ holi.mk
@@ -556,14 +556,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Charger
 PRODUCT_PACKAGES += \
     product_charger_res_images \
