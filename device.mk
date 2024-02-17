ce2c1b9ec8f1f274586e9b630648e3696aa1a5f5
kamikaonashi <thorschi93@gmx.at>
2024-02-17 07:12:41 +0100

    stone: rootdir: introduce init.stone.rc
    to adjust gpu, cpu  boosts, inspiration taken from AOSPA

--- device.mk
+++ device.mk
@@ -525,6 +525,7 @@ PRODUCT_PACKAGES += \
     init.qti.kernel.rc \
     init.qti.kernel.sh \
     init.target.rc \
+    init.stone.rc \
     ueventd.qcom.rc \
     init.xiaomi.rc \
     vendor_modprobe.sh
